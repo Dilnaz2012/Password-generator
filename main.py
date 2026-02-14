17d4d85c7437fadaa4bb9ad454a66bbf0366631f
Dilnaz <you@example.com>
2026-02-14 12:48:38 +0600

    update text

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import random
 
+
 password =  "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-q = int(input("Напишите длину пароля"))
+q = int(input("Напишите длину пароля: "))
 a = ""
 for i in range(q):
     a += random.choice(password)
